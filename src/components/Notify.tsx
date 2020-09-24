30f095e98562347e513ea11da9930f08408dad53
Rik Harink <rik.harink@topicus.nl>
2020-09-24 14:29:41 +0200

    honeypot field

--- src/components/Notify.tsx
+++ src/components/Notify.tsx
@@ -6,13 +6,32 @@ export interface NotifyProps {
 export const Notify: FC<NotifyProps> = ({ className }) => {
   return (
     <div className={className}>
-      <form name="drop-notify" data-netlify="true" data-netlify-honeypot="bot-field" method="post">
+      <form
+        name="drop-notify"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        method="post"
+      >
         <div>
-          <input name="email" className="outline-none border-b bg-transparent" type="text"></input>
+          <input
+            name="email"
+            className="outline-none border-b bg-transparent"
+            type="text"
+          ></input>
           @topicus.nl
         </div>
         <input type="hidden" name="form-name" value="drop-notify" />
-        <button className="rounded-sm border border-white text-white bg-transparent m-6 p-2" type="submit">notify me of next drop</button>
+        <p className="hidden">
+          <label>
+            Don’t fill this out if you're human: <input name="bot-field" />
+          </label>
+        </p>
+        <button
+          className="rounded-sm border border-white text-white bg-transparent m-6 p-2"
+          type="submit"
+        >
+          notify me of next drop
+        </button>
       </form>
     </div>
   );
